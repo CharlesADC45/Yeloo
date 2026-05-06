004e3d46cf24b13355e6bde14ba110dc391c497c
CharlesADC45 <Charles.prince@africadigitalconnect.net>
2026-05-06 15:48:04 +0000

    feat: update service worker and add Workbox support
    - Enhanced the service worker to include new caching strategies and improved precaching of assets.
    - Introduced a new Workbox script for better handling of service worker functionalities.
    - Updated the service worker to utilize the latest Workbox features for optimized performance and reliability.
    - Improved logging capabilities within the service worker for better debugging and monitoring.

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,1 +1,108 @@
-if(!self.define){let e,a={};const s=(s,i)=>(s=new URL(s+".js",i).href,a[s]||new Promise(a=>{if("document"in self){const e=document.createElement("script");e.src=s,e.onload=a,document.head.appendChild(e)}else e=s,importScripts(s),a()}).then(()=>{let e=a[s];if(!e)throw new Error(`Module ${s} didn’t register its module`);return e}));self.define=(i,c)=>{const n=e||("document"in self?document.currentScript.src:"")||location.href;if(a[n])return;let t={};const r=e=>s(e,n),d={module:{uri:n},exports:t,require:r};a[n]=Promise.all(i.map(e=>d[e]||r(e))).then(e=>(c(...e),t))}}define(["./workbox-8232f3e4"],function(e){"use strict";importScripts("fallback-dNMYY8AR9OMcDU8PYa1Y2.js","/notification-worker.js"),self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"/_next/static/chunks/1966.1560d0f43ac9a41a.js",revision:"1560d0f43ac9a41a"},{url:"/_next/static/chunks/2294-6540cebe4fe34efd.js",revision:"6540cebe4fe34efd"},{url:"/_next/static/chunks/3667-3038ef85be7f8dce.js",revision:"3038ef85be7f8dce"},{url:"/_next/static/chunks/3736-008ed255182e6b1a.js",revision:"008ed255182e6b1a"},{url:"/_next/static/chunks/3794-cd98b5cd5fb65b19.js",revision:"cd98b5cd5fb65b19"},{url:"/_next/static/chunks/3899.cf10152d8085e352.js",revision:"cf10152d8085e352"},{url:"/_next/static/chunks/4bd1b696-bf5e0dbacfa5baef.js",revision:"bf5e0dbacfa5baef"},{url:"/_next/static/chunks/6116-38e13a63b5e01dfc.js",revision:"38e13a63b5e01dfc"},{url:"/_next/static/chunks/737-fb2dfad89f3448ca.js",revision:"fb2dfad89f3448ca"},{url:"/_next/static/chunks/7937-1750ad2bed7271b4.js",revision:"1750ad2bed7271b4"},{url:"/_next/static/chunks/8500-41fa79ac743d83f1.js",revision:"41fa79ac743d83f1"},{url:"/_next/static/chunks/9074-d8f95764173da1de.js",revision:"d8f95764173da1de"},{url:"/_next/static/chunks/app/_global-error/page-316ed9a466844a56.js",revision:"316ed9a466844a56"},{url:"/_next/static/chunks/app/_not-found/page-316ed9a466844a56.js",revision:"316ed9a466844a56"},{url:"/_next/static/chunks/app/admin/baux/page-0384f89bde206a81.js",revision:"0384f89bde206a81"},{url:"/_next/static/chunks/app/admin/layout-c88993e4c6f5b7c5.js",revision:"c88993e4c6f5b7c5"},{url:"/_next/static/chunks/app/admin/modules/page-d50f8f5941047c89.js",revision:"d50f8f5941047c89"},{url:"/_next/static/chunks/app/admin/notifications/page-5ae3493f84cdb90b.js",revision:"5ae3493f84cdb90b"},{url:"/_next/static/chunks/app/admin/page-f8acb9ca940d76e7.js",revision:"f8acb9ca940d76e7"},{url:"/_next/static/chunks/app/admin/profil/page-4ba694056b071094.js",revision:"4ba694056b071094"},{url:"/_next/static/chunks/app/admin/publications/page-afb3962b93b4fa7b.js",revision:"afb3962b93b4fa7b"},{url:"/_next/static/chunks/app/admin/users/page-4c06c7eb3751c1f3.js",revision:"4c06c7eb3751c1f3"},{url:"/_next/static/chunks/app/admin/verifications/page-28f48295c5b51add.js",revision:"28f48295c5b51add"},{url:"/_next/static/chunks/app/carte/page-40d176dfd761d9c9.js",revision:"40d176dfd761d9c9"},{url:"/_next/static/chunks/app/compte/aide-support/page-b6d9e0a45c24eca2.js",revision:"b6d9e0a45c24eca2"},{url:"/_next/static/chunks/app/compte/changer-mot-de-passe/page-4547d034a84e5035.js",revision:"4547d034a84e5035"},{url:"/_next/static/chunks/app/compte/informations-personnelles/page-c2b5ad1ec0756ebe.js",revision:"c2b5ad1ec0756ebe"},{url:"/_next/static/chunks/app/compte/offline/page-736c66cb58d9a7de.js",revision:"736c66cb58d9a7de"},{url:"/_next/static/chunks/app/compte/page-1c2e183a16a1a871.js",revision:"1c2e183a16a1a871"},{url:"/_next/static/chunks/app/compte/parametres/page-f2fbce9188db3cad.js",revision:"f2fbce9188db3cad"},{url:"/_next/static/chunks/app/connexion/page-1e5b03fb3a781b80.js",revision:"1e5b03fb3a781b80"},{url:"/_next/static/chunks/app/favoris/page-59e1fdc5454e2c45.js",revision:"59e1fdc5454e2c45"},{url:"/_next/static/chunks/app/inscription/page-d82be36ae6e2280d.js",revision:"d82be36ae6e2280d"},{url:"/_next/static/chunks/app/layout-65fca156292fd7b5.js",revision:"65fca156292fd7b5"},{url:"/_next/static/chunks/app/logements/%5Bid%5D/contrat/page-a33ccafeb6cd0828.js",revision:"a33ccafeb6cd0828"},{url:"/_next/static/chunks/app/logements/%5Bid%5D/page-d9dd4562d82a2f3d.js",revision:"d9dd4562d82a2f3d"},{url:"/_next/static/chunks/app/logements/page-f04be9ce50c8643c.js",revision:"f04be9ce50c8643c"},{url:"/_next/static/chunks/app/messages/%5BconversationId%5D/page-a595339a019f5981.js",revision:"a595339a019f5981"},{url:"/_next/static/chunks/app/messages/page-f6d661129f7d72b9.js",revision:"f6d661129f7d72b9"},{url:"/_next/static/chunks/app/not-found-4961ddc9c5e40bbf.js",revision:"4961ddc9c5e40bbf"},{url:"/_next/static/chunks/app/offline/page-0f0184069f316b54.js",revision:"0f0184069f316b54"},{url:"/_next/static/chunks/app/page-3b4c969adb989984.js",revision:"3b4c969adb989984"},{url:"/_next/static/chunks/app/proprietaire/baux/%5BleaseRequestId%5D/page-ee2b22d3b224c934.js",revision:"ee2b22d3b224c934"},{url:"/_next/static/chunks/app/proprietaire/biens/%5BpropertyId%5D/page-766e71ea0cf796b3.js",revision:"766e71ea0cf796b3"},{url:"/_next/static/chunks/app/proprietaire/biens/nouveau/page-1437fa0cd00ea50e.js",revision:"1437fa0cd00ea50e"},{url:"/_next/static/chunks/app/proprietaire/biens/page-410d42fb755af354.js",revision:"410d42fb755af354"},{url:"/_next/static/chunks/app/proprietaire/calendrier/page-7294cf097f836034.js",revision:"7294cf097f836034"},{url:"/_next/static/chunks/app/proprietaire/layout-316ed9a466844a56.js",revision:"316ed9a466844a56"},{url:"/_next/static/chunks/app/proprietaire/nouveau/page-8a5a02ca5dc8644a.js",revision:"8a5a02ca5dc8644a"},{url:"/_next/static/chunks/app/proprietaire/page-f80c2e1ea6440cb2.js",revision:"f80c2e1ea6440cb2"},{url:"/_next/static/chunks/app/proprietaire/profil/page-7a47734eb43b2030.js",revision:"7a47734eb43b2030"},{url:"/_next/static/chunks/c15bf2b0-94391c50bab8018f.js",revision:"94391c50bab8018f"},{url:"/_next/static/chunks/d0deef33.3b950544dd9967b7.js",revision:"3b950544dd9967b7"},{url:"/_next/static/chunks/ee560e2c-ef4070eb1f9dc766.js",revision:"ef4070eb1f9dc766"},{url:"/_next/static/chunks/framework-f5436bfdb97dbbca.js",revision:"f5436bfdb97dbbca"},{url:"/_next/static/chunks/main-14bbf4d6349da59f.js",revision:"14bbf4d6349da59f"},{url:"/_next/static/chunks/main-app-b3866bfb0b005d23.js",revision:"b3866bfb0b005d23"},{url:"/_next/static/chunks/next/dist/client/components/builtin/app-error-316ed9a466844a56.js",revision:"316ed9a466844a56"},{url:"/_next/static/chunks/next/dist/client/components/builtin/forbidden-316ed9a466844a56.js",revision:"316ed9a466844a56"},{url:"/_next/static/chunks/next/dist/client/components/builtin/global-error-48a43f91874b56f3.js",revision:"48a43f91874b56f3"},{url:"/_next/static/chunks/next/dist/client/components/builtin/unauthorized-316ed9a466844a56.js",revision:"316ed9a466844a56"},{url:"/_next/static/chunks/polyfills-42372ed130431b0a.js",revision:"846118c33b2c0e922d7b3a7676f81f6f"},{url:"/_next/static/chunks/webpack-8f11eedddb94af20.js",revision:"8f11eedddb94af20"},{url:"/_next/static/css/55693049b062c6a3.css",revision:"55693049b062c6a3"},{url:"/_next/static/css/86345be4af5dabfb.css",revision:"86345be4af5dabfb"},{url:"/_next/static/dNMYY8AR9OMcDU8PYa1Y2/_buildManifest.js",revision:"71e5c9029337a0c393fb392ad3bd00b6"},{url:"/_next/static/dNMYY8AR9OMcDU8PYa1Y2/_ssgManifest.js",revision:"b6652df95db52feb4daf4eca35380933"},{url:"/_next/static/media/034d78ad42e9620c-s.woff2",revision:"be7c930fceb794521be0a68e113a71d8"},{url:"/_next/static/media/0484562807a97172-s.p.woff2",revision:"b550bca8934bd86812d1f5e28c9cc1de"},{url:"/_next/static/media/28485c0de2075f40-s.woff2",revision:"58f1bb271968fc16131cba266fce1376"},{url:"/_next/static/media/29a4aea02fdee119-s.woff2",revision:"69d9d2cdadeab7225297d50fc8e48e8b"},{url:"/_next/static/media/29e7bbdce9332268-s.woff2",revision:"9e3ecbe4bb4c6f0b71adc1cd481c2bdc"},{url:"/_next/static/media/5fb25f343c7550ca-s.woff2",revision:"b1ee7ba0b4c946e20d7859cddf2aa203"},{url:"/_next/static/media/7db6c35d839a711c-s.p.woff2",revision:"de2b6fe4e663c0669007e5b501c2026b"},{url:"/_next/static/media/8888a3826f4a3af4-s.p.woff2",revision:"792477d09826b11d1e5a611162c9797a"},{url:"/_next/static/media/a1386beebedccca4-s.woff2",revision:"d3aa06d13d3cf9c0558927051f3cb948"},{url:"/_next/static/media/b957ea75a84b6ea7-s.p.woff2",revision:"0bd523f6049956faaf43c254a719d06a"},{url:"/_next/static/media/c3bc380753a8436c-s.woff2",revision:"5a1b7c983a9dc0a87a2ff138e07ae822"},{url:"/_next/static/media/db911767852bc875-s.woff2",revision:"9516f567cd80b0f418bba2f1299ed6d1"},{url:"/_next/static/media/eafabf029ad39a43-s.p.woff2",revision:"43751174b6b810eb169101a20d8c26f8"},{url:"/_next/static/media/f10b8e9d91f3edcb-s.woff2",revision:"63af7d5e18e585fad8d0220e5d551da1"},{url:"/_next/static/media/fe0777f1195381cb-s.woff2",revision:"f2a04185547c36abfa589651236a9849"},{url:"/_next/static/media/layers-2x.9859cd12.png",revision:"9859cd12"},{url:"/_next/static/media/layers.ef6db872.png",revision:"ef6db872"},{url:"/_next/static/media/marker-icon.d577052a.png",revision:"d577052a"},{url:"/icons/address.png",revision:"81f931775d576119d673ac26e0bf12f3"},{url:"/icons/apple-touch-icon.png",revision:"601768dcd39946104aee1b551d1b1508"},{url:"/icons/icon-192.png",revision:"40cda440f4bc135285a7240ce833b4e4"},{url:"/icons/icon-512.png",revision:"cf10cf0bde50f83277876507cb850759"},{url:"/icons/maskable-icon-512.png",revision:"0e1992b67be34989fe28c9c6976d510f"},{url:"/icons/menu.png",revision:"7f387f8933f1a5c1adb34d99c44fb58e"},{url:"/manifest.webmanifest",revision:"fc45c60a5e57595ffaf064e14eb3c05c"},{url:"/notification-worker.js",revision:"8dcfaf00d3628d64a4750e407347e8ea"},{url:"/offline",revision:"dNMYY8AR9OMcDU8PYa1Y2"},{url:"/property-fallback.svg",revision:"7c5f7ec8f32cefbb28c200fecef7b142"},{url:"/vendor/pannellum/pannellum.css",revision:"79afb679a01912154e8eabfe06d81663"},{url:"/vendor/pannellum/pannellum.js",revision:"a5a9fdcea1a7ddb21ae1be07e62dbac3"}],{ignoreURLParametersMatching:[]}),e.cleanupOutdatedCaches(),e.registerRoute("/",new e.NetworkFirst({cacheName:"start-url",plugins:[{cacheWillUpdate:async({request:e,response:a,event:s,state:i})=>a&&"opaqueredirect"===a.type?new Response(a.body,{status:200,statusText:"OK",headers:a.headers}):a},{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET"),e.registerRoute(/^https:\/\/[abc]\.tile\.openstreetmap\.org\/.*$/i,new e.CacheFirst({cacheName:"yeloo-osm-map-tiles",plugins:[new e.ExpirationPlugin({maxEntries:900,maxAgeSeconds:604800}),new e.CacheableResponsePlugin({statuses:[0,200]}),{handlerDidError:async({request:e})=>self.fallback(e)}]}),"GET")});
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-e43f5367'], (function (workbox) { 'use strict';
+
+  importScripts("fallback-development.js", "/notification-worker.js");
+  self.skipWaiting();
+  workbox.clientsClaim();
+  workbox.registerRoute("/", new workbox.NetworkFirst({
+    "cacheName": "start-url",
+    plugins: [{
+      cacheWillUpdate: async ({
+        request,
+        response,
+        event,
+        state
+      }) => {
+        if (response && response.type === 'opaqueredirect') {
+          return new Response(response.body, {
+            status: 200,
+            statusText: 'OK',
+            headers: response.headers
+          });
+        }
+        return response;
+      }
+    }, {
+      handlerDidError: async ({
+        request
+      }) => self.fallback(request)
+    }]
+  }), 'GET');
+  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
+    "cacheName": "dev",
+    plugins: [{
+      handlerDidError: async ({
+        request
+      }) => self.fallback(request)
+    }]
+  }), 'GET');
+
+}));
